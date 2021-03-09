f372a02aa567d6f8bc9b5a48fe97a5cddb8e1122
Ethan C <ethan.caya@uconn.edu>
2021-03-08 22:10:25 -0500

    Added support for writing JSON file for database. Created PatientProf.

--- src/PatientProf.java
+++ src/PatientProf.java
@@ -1,23 +1,96 @@
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
+
+@SuppressWarnings("ALL")
 public class PatientProf{
     private String adminID;
     private String firstName;
     private String lastName;
     private String phone;
     private Float coPay;
+    //insuType is "Private" or "Government"
     private String insuType;
+    //patientType is "Pediatric", "Adult", or "Senior"
     private String patientType;
     private MedCond medCondInfo;
+
+    public PatientProf(String adminID, String firstName, String lastName,
+                       String phone, Float coPay, String insuType,
+                       String patientType, @Nullable MedCond medCondInfo) throws RuntimeException{
+        this.adminID = adminID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.coPay = coPay;
+        //check insuType is correct
+        insuType = insuType.toLowerCase(Locale.ROOT).strip();
+        if(insuType.equals("private") | insuType.equals("government")){
+            this.insuType = insuType;
+        }
+        else{
+            throw new RuntimeException("Insurance type should be 'Private'" +
+                    " or 'Government' not " + insuType);
+        }
+
+        //check patientType is correct
+        patientType = patientType.toLowerCase(Locale.ROOT).strip();
+        if(patientType.equals("pediatric") | patientType.equals("adult")
+                | patientType.equals("senior")){
+            this.patientType = patientType;
+        }
+        else{
+            throw new RuntimeException("Patient type should be 'Pediatric', " +
+                    "'Adult', or 'Senior' not " + patientType);
+        }
+        this.medCondInfo = medCondInfo;
+    }
+
 
     /**
      * Equality method for comparing PatientProf.
      * Needed to check for duplicates in PatientDB.
-     * @param p1 1st PatientProf to check
-     * @param p2 2nd PatientProf to check
+     * @param patient the patient to check against
      * @return True if lastName and adminID match
      */
-    public boolean equals(PatientProf p1, PatientProf p2){
-        return((p1.adminID.equals(p2.adminID))
-                & (p1.lastName.equals(p2.lastName))
+    public boolean equals(PatientProf patient){
+        return((this.adminID.equals(patient.adminID))
+                & (this.lastName.equals(patient.lastName))
         );
     }
+
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Float getCoPay() {
+        return coPay;
+    }
+
+    public String getInsuType() {
+        return insuType;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public @Nullable MedCond getMedCondInfo() {
+        return medCondInfo;
+    }
+
+
 }
--- src/PatientProfDB.java
+++ src/PatientProfDB.java
@@ -1,5 +1,7 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -8,11 +10,12 @@ public class PatientProfDB{
     private int currentPatientIndex;
     private String fileName;
     private final List<PatientProf> patientList = new ArrayList<PatientProf>();
-    private final String EXTENSION = "csv";
+    private final String EXTENSION = ".json";
+
 
     public PatientProfDB(String filePath) throws IOException, RuntimeException {
         //check if correct type of file
-        if(checkFileExtension(filePath, EXTENSION)){
+        if(!checkFileExtension(filePath, EXTENSION)){
             throw new RuntimeException("Expected a " + EXTENSION
                     + " file.");
         }
@@ -24,20 +27,23 @@ public class PatientProfDB{
 
         //set read/write permission
         if(!DBFile.setReadable(true)){
-            throw new IOException("No read permission for DB file");
+            throw new RuntimeException("No read permission for DB file");
         }
-        if(DBFile.setWritable(true)){
-            throw new IOException("No write permission for DB file");
+        if(!DBFile.setWritable(true)){
+            throw new RuntimeException("No write permission for DB file");
         }
         //if existing DB file read in the contents
-        if(newFile){
+        if(!newFile){
             initializeDatabase(filePath);
         }
+        //TODO call interface method to prompt new database creation
+        //call writeDatabase if wanted
     }
 
+
     /**
-     * Scans the contents of a DBfile into the DB.
-     * @param filePath The filepath of a database csv file
+     * Scans the contents of a JSON PatientDB file into the DB.
+     * @param filePath The filepath of a database JSON file
      */
     public void initializeDatabase(String filePath){
         Scanner scanner = new Scanner(filePath);
@@ -48,16 +54,100 @@ public class PatientProfDB{
 
     }
 
+
     /**
-     * Checks the file extension and throws an exception
+     * Writes the PatientDB to the specified filepath as a JSON file.
+     * @param filepath the desired filepath of the JSON file
+     */
+    private void writeDatabase(String filepath) throws RuntimeException{
+        File databaseFile = new File(filepath);
+        //create file
+        try {
+            if (!databaseFile.createNewFile()) {
+                throw new RuntimeException("Couldn't write database file at "
+                        + filepath + ". File already exists with that name");
+            }
+        }
+        catch(IOException e){
+            throw new RuntimeException("Couldn't write database file " + e);
+        }
+        //check writable
+        if(!databaseFile.setWritable(true)){
+            throw new RuntimeException("No write permission for the document");
+        }
+
+        //create a fileWriter to write to the created file
+        FileWriter writer;
+        try{
+            writer = new FileWriter(databaseFile);
+        }
+        catch (IOException e){
+            throw new RuntimeException("Couldn't create FileWriter" + e);
+        }
+
+        //construct JSONArray to write to file
+        JSONArray patientJsonArray = constructPatientJsonArray(this.patientList);
+
+        //write file
+        try {
+            writer.write(patientJsonArray.toString(4));
+        }
+        catch(IOException e){
+            throw new RuntimeException("Failed to write database file " + e);
+        }
+
+    }
+
+
+    /**
+     * Checks if the provided filename has the given file extension.
+     * Does not handle illegal filenames.
      * @param fileName the file name to check the extension of
      * @param extension the file extension to check against
      * @return True if the file extension matches
      */
     private boolean checkFileExtension(String fileName, String extension){
         fileName = fileName.toLowerCase();
-        String[] splitPath = fileName.split(".");
+        String[] splitPath = fileName.split("\\.");
         String fileExtension = splitPath[splitPath.length - 1];
         return(fileExtension.equals(extension));
+    }
+
+
+    /**
+     * Constructs a JSONArray that contains all patients in the patientList
+     * @param patientList List of PatientProfs
+     * @return a JSONArray containing all patients in patientList
+     */
+    private JSONArray constructPatientJsonArray(List<PatientProf> patientList){
+        JSONArray dbArray = new JSONArray();
+        //iterate over patients in DB
+        for(PatientProf patient: this.patientList){
+            //create object for each patient
+            JSONObject patientObj = new JSONObject();
+            //fill out patient information
+            patientObj.put("adminID", patient.getAdminID());
+            patientObj.put("firstName", patient.getFirstName());
+            patientObj.put("lastName", patient.getLastName());
+            patientObj.put("phone", patient.getPhone());
+            patientObj.put("coPay", patient.getCoPay());
+            patientObj.put("insuType", patient.getInsuType());
+            patientObj.put("patientType", patient.getPatientType());
+
+            //create medCondObj to add to patientObj
+            JSONObject medCondObj = new JSONObject();
+            MedCond medCond = patient.getMedCondInfo();
+            medCondObj.put("mdContact", medCond.getMdContact());
+            medCondObj.put("mdPhone", medCond.getMdPhone());
+            medCondObj.put("algType", medCond.getAlgType());
+            medCondObj.put("illType", medCond.getIllType());
+            //put the medCondObj into patientObj
+            patientObj.put("medCondInfo", medCondObj);
+
+            //add patient object to dbArray
+            dbArray.put(patientObj);
+        }
+
+        return dbArray;
     }
 }
